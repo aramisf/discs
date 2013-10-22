49aa61a828aae20a6dd826a418be78b509d6e269
aramisf <ashf03@c3sl.ufpr.br>
2013-10-22 15:32:50 -0200

    Adicionada funcao de calculo dos vetores.

--- geometria_computacional/2trab/molde2d.pl
+++ geometria_computacional/2trab/molde2d.pl
@@ -5,23 +5,45 @@ use strict;
 use warnings;
 
 
-########################
-## Variaveis globais: ##
-########################
+#########################
+## ariaveis globais: ##
+#########################
 
 my $n;          # num de pontos a serem lidos
 my $pontos;     # lista de pontos lidos
+my $vetores;    # vetores das retas (B-A)
 
 
-#########################
-## Programa principal: ##
-#########################
+####################
+## erramentas : ##
+####################
+
+sub calcula_vetores {
+
+  for (my $i=0; $i < @$pontos-1; $i++) {
+
+    push @$vetores, [
+                      $$pontos[$i+1][0] - $$pontos[$i][0],
+                      $$pontos[$i+1][1] - $$pontos[$i][1]
+                    ];
+  }
+
+  push @$vetores, [
+                    $$pontos[0][0] - $$pontos[@$pontos-1][0],
+                    $$pontos[0][1] - $$pontos[@$pontos-1][1]
+                  ];
+
+} # /calcula_vetores
+
+##########################
+## rograma principal: ##
+##########################
 
 chomp ($n = <STDIN>);             # Numero de pontos
 for (my $i = 0; $i < $n; $i++) {  # Pontos com duas coordenadas
 
   chomp($_ = <STDIN>);
-  push @$pontos, [split] ;
+  push @$pontos, [split];
 }
 
-
+calcula_vetores();
