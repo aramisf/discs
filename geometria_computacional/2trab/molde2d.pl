6bd956dbc57a9b5daa6f13fa5a8ac57aa8de4d50
aramisf <ashf03@c3sl.ufpr.br>
2013-12-10 18:29:02 -0200

    Terminado molde 2d.
    - Reduzido o numero de operacoes aritmeticas;
    - Melhorada a clareza do codigo;
    - Removidas as msgs de debug;
    - Corrigido erro aritmetico.

--- geometria_computacional/2trab/molde2d.pl
+++ geometria_computacional/2trab/molde2d.pl
@@ -41,33 +41,28 @@ sub calcula_vetores { # e seus respectivos vetores normais
 
     # Dados os pontos A e B, o vetor AB eh calculado como B - A. Eh isso que
     # esta sendo feito aqui, e cada AB BC CD eh empilhado em @vetores
-    push @$vetores, [
-                      $$pontos[$i+1][0] - $$pontos[$i][0],
-                      $$pontos[$i+1][1] - $$pontos[$i][1]
-                    ];
+
+    # Coordenadas do vetor AB:
+    my $x = $$pontos[$i+1][0] - $$pontos[$i][0];
+    my $y = $$pontos[$i+1][1] - $$pontos[$i][1];
+
+    push @$vetores, [$x, $y];
 
     # Conforme a matriz de rotacao, ao rotacionar um vetor com as coordenadas
     # (x,y), temos as coordenadas (-y,x) ou (y,-x), no caso, os pontos foram
     # lidos no sentido antihorario, entao (y,-x) aponta para o centro do
     # poligono, pois queremos rotacionar os vetores no sentido antihorario
     # (-90o) http://pt.wikipedia.org/wiki/Matriz_de_rota%C3%A7%C3%A3o
-    push @$normais, [
-                      $$pontos[$i][1] - $$pontos[$i+1][1],
-                      -($$pontos[$i][0] - $$pontos[$i+1][0])
-                    ];
-
+    push @$normais, [$y, -$x];
   }
 
   # Esse aqui eh o ultimo, por exemplo, se fossem pontos A, B, C e D, este aqui
-  # seria o vetor DA
-  push @$vetores, [
-                    $$pontos[0][0] - $$pontos[@$pontos-1][0],
-                    $$pontos[0][1] - $$pontos[@$pontos-1][1]
-                  ];
-  push @$normais, [
-                    $$pontos[0][1] - -$$pontos[@$pontos-1][1],
-                    -($$pontos[0][0] - -$$pontos[@$pontos-1][0])
-                  ];
+  # seria o vetor DA (que eh A - D)
+  my $x = $$pontos[0][0] - $$pontos[-1][0];
+  my $y = $$pontos[0][1] - $$pontos[-1][1];
+
+  push @$vetores, [$x, $y];
+  push @$normais, [$y, -$x];
 
 } # /calcula_vetores
 
@@ -84,21 +79,26 @@ sub percorre_faces {
 
     my $deu_boa = 1;  # assumo que vai dar boa
 
-    #print "Lendo face: @u\n";
     # Testa se ele tem no maximo 90 graus de angulo com as outras faces:
     for (my $j = $i+1; $j < @$normais; $j++) {
 
       # cos(teta) = u.v/|u|.|v|
       my @v = @{$$normais[$j]};
-      print "cosseno_de_teta(@u, @v)\n";
       my $cos = cosseno_de_teta(\@u, \@v);
-      #print "$cos\n";
-      $deu_boa = 0 and last if ($cos < 0);
+
+      # Se der erro com o vetor atual, interrompe os testes para a face corrente.
+      if ($cos < 0) {
+
+        $deu_boa = 0;
+        last;
+      }
+
     }
 
-    if ($deu_boa) {
+    if ($deu_boa && $i != @$normais-1) {
       return $i+1;  # +1 pq a numeracao das faces comeca com 1
     }
+
   }
 
   return 0;
@@ -117,18 +117,5 @@ for (my $i = 0; $i < $n; $i++) {  # Pontos com duas coordenadas
 }
 
 calcula_vetores();
-
-print "Vetores:\n";
-for (@$vetores) {
-
-  print "@$_\n";
-}
-
-print "Normais:\n";
-for (@$normais) {
-
-  print "@$_\n";
-}
-
 print percorre_faces();
 exit 0;
