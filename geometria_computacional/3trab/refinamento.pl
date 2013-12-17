c145462ae937ded7422d224298b4153d63587ff4
aramisf <ashf03@c3sl.ufpr.br>
2013-12-17 12:20:53 -0200

    Adicionada funcao q trata as primeiras infos.

--- geometria_computacional/3trab/refinamento.pl
+++ geometria_computacional/3trab/refinamento.pl
@@ -11,10 +11,40 @@ use warnings;
 my $n;  # Num de vertices da triangulacao   Tipo: inteiro
 my $m;  # Num de triangulos                 Tipo: inteiro
 
-my $nuvem       # Nuvem de pontos           Tipo: referencia para uma hash de pares
+my $DCEL  = {}; # DCEL                      Tipo: referencia para uma hash de pares
                 #                                 ordenados (tamanho: $n),
                 #                                 indexada por inteiros
 
+my $vertices;   # Adivinha..
+my $arestas;    # ^^
 
 
-# Lendo dados da entrada padrao:
+###########
+## Utils ##
+###########
+
+# Le entrada:
+sub jessica {
+
+  # Lendo num de vertices e triangulos:
+  ($n, $m) = split ' ', <>;
+
+
+  # Pra que gastar mais de uma linha de codigo para ler os vertices e empilhar
+  # suas coordenadas em um vetor?
+  push @$vertices, [ split ' ', <> ] for (1..$n);
+
+
+  # E agora, os vertices que compoe cada triangulo, e os vizinhos de cada
+  # triangulo:
+
+}
+
+########################
+## Programa principal ##
+########################
+
+jessica();
+
+print "@$_\n" for (@$vertices);
+
