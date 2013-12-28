f17960157f1b08a1b681e60dfea0f661951efc9b
aramisf <ashf03@c3sl.ufpr.br>
2013-12-28 21:38:57 -0200

    [geom_comp][4trab] Correcao no comentario.
    E edicao das msg de commit, com tags (que deviam ter sido usadas desde o comeco)

--- geometria_computacional/4trab/quickhull3d.pl
+++ geometria_computacional/4trab/quickhull3d.pl
@@ -16,7 +16,8 @@ my $vertices;     # Hash indexada de vertices
 
 # Le os dados da entrada padrao e monta a lista indexada de vertices. As chaves
 # sao inteiros q indexam a lista, e cada chave possui um array anonimo como
-# valor, contento as 3 coordenadas do vetor indexado por $linha.
+# valor, contento as 3 coordenadas do vetor indexado pelo numero da linha sendo
+# lida.
 sub arya {
 
   ${$vertices}{$.} = [split] while <>;
