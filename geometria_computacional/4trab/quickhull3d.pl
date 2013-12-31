88258af0a3f3c2a2c4ed89e8109072fd86a8b7f1
aramisf <ashf03@c3sl.ufpr.br>
2013-12-31 20:44:41 -0200

    [geom_comp][4trab] Ultimo commit do ano!
    Encontrando os vertices para o simplexo:
    
    Adicionada parte das funcoes que encontram os vertices candidatos para montar o
    simplexo inicial. E mais algumas funcoes auxiliares.
    
    O proximo passo consiste em calcular os planos das faces e verificar se existem
    vertices fora do poliedro. Caso existam, inclui-los e recalcular as novas faces.

--- geometria_computacional/4trab/quickhull3d.pl
+++ geometria_computacional/4trab/quickhull3d.pl
@@ -30,7 +30,25 @@ sub arya {
   ${$vertices}{$.} = [split] while <>;
 }
 
-# Seleciona vetores candidatos a extremos para o simplexo inicial:
+# Retorna o menor entre 3 valores quaisquer:
+sub theon {
+
+  my ($a, $b, $c) = @_;
+
+  if ($b >= $c) {
+
+    if ($a >= $b) { 0;} else { 1;}
+
+  } else {
+
+    if ($a >= $c) { 0;} else { 2;}
+  }
+
+} #/theon
+
+# Seleciona vetores candidatos a extremos para o simplexo inicial. Retorna uma
+# lista com 6 (identificadores de) vertices: menor x, maior x, menor y, maior
+# y, menor z e maior z, nesta ordem.
 sub petyr {
 
   # Indice 0 -> rotulo do vertice;
@@ -89,14 +107,214 @@ sub petyr {
   # menor x, maior x,
   # menor y, maior y,
   # menor z, maior z.
-
-  return  [ $min_x[0],$max_x[0],
-            $min_y[0],$max_y[0],
-            $min_z[0],$max_z[0],
-          ];
+  [
+    $min_x[0],$max_x[0],
+    $min_y[0],$max_y[0],
+    $min_z[0],$max_z[0],
+  ];
 
 } #/petyr
+
+# Calcula a hipotenusa.
+sub ros {
+
+  my ($x, $y, $z) = @_;
+
+  $x*$x + $y*$y + $z*$z;
+
+} #/ros
+
+# Normaliza um vetor passado como parametro e retorna o resultado:
+sub sansa {
+
+  my ($x, $y, $z) = @_;
+  my $hip   = ros(@_);
+  my $sqrt  = sqrt $hip;
+
+  ($x/$sqrt, $y/$sqrt, $z/$sqrt);
+
+} #/sansa
+
+# Retorna o produto vetorial de dois vetores passados como parametro:
+sub shae {
+
+  my $u = shift;
+  my $v = shift;
+
+  (
+    @{$v}[0] * @{$u}[0],
+    @{$v}[1] * @{$u}[1],
+    @{$v}[2] * @{$u}[2]
+  );
+
+} #/shae
+
+# Retorna o produto escalar de dois vetores passados como parametro:
+sub podrick {
+
+  my $u = shift;
+  my $v = shift;
+
+  @{$v}[0] * @{$u}[0] +
+  @{$v}[1] * @{$u}[1] +
+  @{$v}[2] * @{$u}[2];
+
+} #/podrick
+
+# Retorna o vetor resultante da subtracao entre dois vertices:
+sub cersei {
+
+  my $u = shift;
+  my $v = shift;
+
+  (
+    @{$v}[0] - @{$u}[0],
+    @{$v}[1] - @{$u}[1],
+    @{$v}[2] - @{$u}[2],
+  );
+} #/cersei
+
+# Encontra os dois vertices mais distantes entre si, considerando os eixos.
+# Retorna o par que fara parte do primeiro simplexo.
+sub tyrion {
+
+  my $simplexo;           # Simplexo inicial
+  my $extremos = petyr(); # Pontos extremos nos eixos -> candidatos iniciais
+
+  # Vetores para compor o simplexo (que contera os vertices A, B, C e D):
+  my @ab;
+  my @ac;
+  my @av;                 # iterador no loop
+
+  # Produtos escalares de alguns vetores q vamos usar:
+  my $prod_ab_ac;
+  my $prod_ac_av;
+
+  # Produtos vetoriais:
+  my @prod_ab_ac;     # normal entre os vetores AB e AC
+  my @prod_ab_av;     # auxiliar para percorrer lista de pontos
+
+
+  # Extraindo coordenadas:
+  my $min_x = ${$vertices}{@{$extremos}[0]}[0];
+  my $max_x = ${$vertices}{@{$extremos}[1]}[0];
+
+  my $min_y = ${$vertices}{@{$extremos}[2]}[1];
+  my $max_y = ${$vertices}{@{$extremos}[3]}[1];
+
+  my $min_z = ${$vertices}{@{$extremos}[4]}[2];
+  my $max_z = ${$vertices}{@{$extremos}[5]}[2];
+
+
+  # Qual o eixo cujos extremos possuem maior distancia entre si?
+  my $xDist = $max_x - $min_x;
+  my $yDist = $max_y - $min_y;
+  my $zDist = $max_z - $min_z;
+
+
+  # Theon retorna o maior entre 3 valores, que aqui me servirao para saber qual
+  # dos eixos eh o escolhido
+  my $xyz   = theon($xDist,$yDist,$zDist);
+
+  if    ($xyz == 0) {
+
+    push @$simplexo, (@{$extremos}[0], @{$extremos}[1]);
+  }
+  elsif ($xyz == 1) {
+
+    push @$simplexo, (@{$extremos}[2], @{$extremos}[3]);
+  }
+  elsif ($xyz == 2) {
+
+    push @$simplexo, (@{$extremos}[4], @{$extremos}[5]);
+  }
+
+  # Ateh aqui temos os 2 1os vertices
+
+  # $a e $b sao referencias para os vetores.
+  my $a = ${$vertices}{@$simplexo[1]};
+  my $b = ${$vertices}{@$simplexo[0]};
+
+  # Calculando vetor AB:
+  @ab   = cersei($a,$b);
+
+  # Normalizando, para nao afetar o tamanho dos outros vetores que serao
+  # testados no loop q segue
+  @ab   = sansa(@ab);
+
+  # TODO: Refatorar colocando em uma funcao?
+  # Agora um loop p buscar o vertice mais distante de A, diferente do vertice B
+  # ja escolhido. Chamemos este vertice de $v, e seja ele diferente de B.
+  my $maiorTam  = 0;
+  for my $v (keys %$vertices) {
+
+    # Testando apenas nos vertices ainda nao selecionados:
+    unless ($v ~~ @$simplexo) {
+
+      my $candidato   = ${$vertices}{$v};
+
+      # Calcula AV
+      @av             = cersei($a,$candidato);
+
+      # Calcula produto vetorial entre AB e AC:
+      my @prod_ab_av  = shae(\@ab,\@av);
+
+      # E por fim a distancia (hipotenusa**2)
+      my $tam         = ros(@prod_ab_av);
+
+      # Se for maior, atualiza e elege o candidato (aqui referenciado apenas
+      # com o indice)
+      if ($tam > $maiorTam) {
+
+        $maiorTam     = $tam;
+        @$simplexo[2] = $v;
+        @prod_ab_ac   = @prod_ab_av;
+      }
+    }
+  } #/for my $v (%$vertices)
+
+  # Normaliza para encontrar o 4o vertice:
+  @prod_ab_ac = sansa(@prod_ab_ac);
+
+  # Agora partimos para a busca do ultimo vertice que fara parte do simplexo
+  # inicial.
+  my $maxDist = 0;
+  my $dist    = podrick(\@prod_ab_ac,${$vertices}{@$simplexo[2]});
+
+  # Novamente percorremos a lista de vertices:
+  for my $v (keys %$vertices) {
+
+    unless ($v ~~ @$simplexo) {
+
+      # calcula o produto escalar do vertice corrente com @prod_ab_ac, e
+      # subtrai de $dist
+      my $candidato   = ${$vertices}{$v};
+      my $distNova    = podrick(\@prod_ab_ac,$candidato) - $dist;
+
+      if ($distNova > $maxDist) {
+
+        $maxDist      = $distNova;
+        @$simplexo[3] = $v;
+      }
+    }
+  } #/for my $v (keys %$vertices)
+
+  print "Eleitos ateh o momento: @$simplexo\n";
+
+  # TODO: Criar as faces
+
+} #/tyrion
+
+# Proximos passos:
+#
+# Percorrer as faces do simplexo, buscando vertices 'visiveis' pela face.
+# Remover a face corrente, e criar outras tres, entre o vertice inserido e os 3
+# vertices que compunham a face recem excluida.
+# Fazer isso enquanto houverem vertices fora do poliedro.
+
 
 ##########
 ## Main ##
 ##########
+arya();
+tyrion();
