746c016d403a4c633166d4994f3c519817efb019
aramisf <ashf03@c3sl.ufpr.br>
2014-04-11 18:34:40 -0300

    [cripto] Adicionado executavel.
    Ainda em sua versao de testes.

---
+++ criptografia/1trab/pf.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+require "lib/Playfair.pm";
+
+# FileHandles:
+my $texto_cifrado;                    # Sera lido da entrada, 1o parametro
+my $dicionario;                       # Idem ^, 2o parametro
+
+
+# String gigante contendo o texto inteiro cifrado
+my $texto_cifrado_str;
+
+
+if (@ARGV < 2) {  # Se for maior nao da nada, vo ignorar do 3o parametro em
+                  # diante
+
+  print "Uso:\n\t";
+  print "$0 <arquivo com o texto cifrado> <arquivo com a lista de chaves>\n\n";
+  exit 1;
+}
+
+else {
+
+  if (-f $ARGV[0]) {    # Diferente do C, $ARGV[0] contem o 1o parametro
+                        # passado para o programa
+
+    open ($texto_cifrado, "<", $ARGV[0]) or
+      die "Erro ao abrir $ARGV[0]: $!\n";
+
+    open ($dicionario, "<", $ARGV[1]) or
+      die "Erro ao abrir $ARGV[1]: $!\n";
+  }
+}
+
+# Lendo o texto cifrado e armazenando ele em uma string soh, potencialmente
+# gigante.
+while (<$texto_cifrado>) {
+
+  s/\s+//g;
+  $texto_cifrado_str .= $_;
+}
+
+# Agora percorre a lista de chaves
+while (my $chave  = <$dicionario>) {
+
+  # TODO: Removendo espacos em branco, mas tem um jeito melhor de fazer isso
+  $chave  =~ s/\s+//g;
+  open(my $arq, ">", "resultados/".$chave);
+
+  # Menos processamento e mais memoria
+  Playfair::decrypt($chave,$texto_cifrado_str,$arq);
+
+  close $arq;
+}
+
+exit 0;
