d3ae44b95420a6e2679436d38f5d3069708af785
aramisf <ashf03@c3sl.ufpr.br>
2014-04-15 17:17:33 -0300

    [cripto] Adicionando bkp dos dados.

--- criptografia/1trab/lib/Playfair.pm
+++ criptografia/1trab/lib/Playfair.pm
@@ -1,56 +1,170 @@
 #!/usr/bin/perl
 
-package Playfair;   # TODO: To pensando se faco um modulo ou uma classe, mas
-                    # acho q vai ser um modulo mesmo
-
 use strict;
 use warnings;
 
+# Criando um modulo importavel, nao vo exportar nada aqui com o intuiro de
+# manter o namespace limpo, jah que o programa usara forca bruta.
+#
+package Playfair;
+
+our $matriz;    # Matriz do playfair, montada pelo gera_matriz, e utilizada
+                # por algumas funcoes auxiliares.
+
 # Gera o conjunto de intervalos de caracteres para gerar as chaves por forca
 # bruta. Os intervalos vao de 'a' a 'z', de 'aa' a 'zz' e assim por diante,
-# ateh atingir o tamanho informado como parametro.
+# ateh atingir o tamanho informado como parametro. Esta funcao eh meramente
+# uma ferramenta para a funcao que criara a lista de chaves para a forca
+# bruta.
 #
 # Parametros: um inteiro indicando a quantidade de inteiros na chave
-# Saida: retorna o array com as meta-chaves
+# Saida: retorna referencia para um array com as meta-chaves
 #
 sub gera_meta_chaves {
 
-  # Se for uma classe
-  #my $self    = $_[0];
-  #my $maxlen  = $_[1];
-
-  # Mas se for um modulo:
   my $maxlen  = shift;
-
   my @meta_chaves;
+
   push @meta_chaves, ['a'x$_,'z'x$_] for 1..$maxlen;
 
-  @meta_chaves;
+  \@meta_chaves;
+}
+
+# Gera um conjunto de chaves, utilizando a funcao anterior como ferramenta.
+# Esta funcao oferece uma funcionalidade que o usuario possa querer, caso nao
+# possua um dicionario de palavras.
+#
+# Parametros: um inteiro indicando o tamanho maximo de cada chave
+# Saida: um arquivo texto contendo o conjunto de chaves.
+#
+sub gera_chaves {
+
+  my $tam         = shift;
+  my $meta_chaves = gera_meta_chaves($tam);
+
+  # Abrindo o arquivo de saida para escrever a lista
+  open(my $saida, ">", "lista_forca_bruta.txt") or
+    die "Erro ao abrir o arquivo de saida: $!\n";
+
+  for my $ref (@$meta_chaves) {
+
+    my $inicio  = $$ref[0];
+    my $fim     = $$ref[1];
+
+    # Adicionando tratamento de chaves, omitindo palavras com caracteres
+    # repetidos, para uma lista menor e para evitar a necessidade de tratar
+    # caracteres recorrentes (no caso especifico do playfair):
+    for ($inicio..$fim) {
+      next if /(.).*?\1/;
+      print $saida "$_\n";
+    }
+  }
+
+  close $saida;
+}
+
+# Gera uma matriz de decriptografia, dada uma string como chave
+#
+# Parametros: uma string que sera considerada a chave de decriptografia
+# Saida: uma matriz representada na forma de uma string.
+#
+sub gera_matriz {
+
+  my $chave     = lc shift;
+  my $alfabeto  = join '', ('a'..'i','k'..'z'); # j == i
+
+  # Removendo caracteres repetidos na chave:
+  1 while $chave =~ s/(.)(.*?)\1+/$1$2/g;
+
+  # Uma vez que j == i, pois ocupam o mesmo lugar na matriz, vamos trocar
+  # todos os j's por i's, para economizar alguns if's no futuro:
+  $chave        =~ s/j/i/g;
+
+  $matriz    = $chave;
+
+  # Remove da lista de caracteres os caracteres jah presentes na chave:
+  $alfabeto     =~ s/[$chave]//g;
+  $matriz       .= $alfabeto;
+
+  #$matriz;   # Como ela eh uma variavel global, posso omitir daqui.
 }
 
 # Decifra um texto global, usando uma chave passada como parametro.
 #
-# TODO: ver um esquema para gerar a matriz de decifragem.
+# Parametros: - a chave a ser utilizada para decifrar o texto;
+#             - uma string sem espacos entre os caracteres;
+#             - um ponteiro para arquivo, que aqui eh assumido como ja aberto
+#               para escrita;
 #
-# Parametros: uma string (que pode ser um texto gigante)
-# Saida:      um arquivo cujo nome eh a chave utilizada para decriptografar o
-#             texto passado como parametro.
+# Saida:      um arquivo cujo nome eh a chave utilizada para decifrar o texto
+#             passado como parametro.
 sub decrypt {
+
+  my ($chave,
+      $texto_cifrado,
+      $arquivo_de_saida)      = @_;
+
+  gera_matriz($chave);
+
+  open(my $arq, ">", "$arquivo_de_saida") or
+    die "Erro ao abrir $arquivo_de_saida: $!";
 
   # - Subdividir a string dois a dois caracteres, tratando os casos de
   #   duplicidade;
-  my $texto_claro = shift;
+  $_                          = $texto_cifrado;
 
-  # - fazer a decriptografia baseando-se na matriz de decriptografia atual;
+  while (/(.)(.)/g) {
 
-  # Imprimir o resultado em um arquivo:
+    my ($um, $dois) = ($1,$2);
+    my $cont         = 1;      # Contador de linhas, p formatacao da saida.
+
+    # Examinando $um e $dois:
+    #
+    # x -> linha
+    # y -> coluna
+    my ($pos1,$pos2);
+    my ($x1,$y1,$x2,$y2)  =
+      (
+        int((index $matriz,$um)   / 5),
+            (index $matriz,$um)   % 5,
+        int((index $matriz,$dois) / 5),
+            (index $matriz,$dois) % 5,
+      );
+
+    # - fazer a decriptografia baseando-se na matriz de decriptografia atual;
+    # Mesma linha
+    if ($x1 == $x2) {
+
+      # move aa esquerda
+      $y1   = --$y1 % 5;
+      $y2   = --$y2 % 5;
+      $pos1 = ($x1*5 + $y1);
+      $pos2 = ($x2*5 + $y2);
+    }
+    elsif ($y1 == $y2) {
+      # move acima
+      $x1   = --$x1 % 5;
+      $x2   = --$x2 % 5;
+      $pos1 = ($x1*5 + $y1);
+      $pos2 = ($x2*5 + $y2);
+    }
+    else {
+      #troca ambos
+      $pos1 = ($x1*5 + $y2);
+      $pos2 = ($x2*5 + $y1);
+    }
+
+    $um   = substr $matriz,$pos1,1;
+    $dois = substr $matriz,$pos2,1;
+
+    # Imprimir o resultado em um arquivo:
+    print $arq "$um$dois ";
+    print $arq "\n" if $cont++ == 80;
+  }
+
+  print $arq "\n";
+  close $arq;
   "Dae\n";
 }
 
-# TODO:
-# Parametros: uma string que corresponde aa chave de criptografia
-# Saida:      a matriz, que deve ser armazenada no proprio objeto
-sub gera_matriz {
-
-}
 1;
--- criptografia/1trab/t/prove_playfair.t
+++ criptografia/1trab/t/prove_playfair.t
@@ -5,34 +5,64 @@ use warnings;
 
 use Test::More;
 
-# Na documentacao diz que eh interessante colocar esses blocos BEGIN aqui, mas
-# mesmo com eles comentados os testes estao passando, entao vou deixar aqui
-# apenas como lembrete para o futuro.
-#BEGIN {
-  require_ok("Playfair");   # Testa se o modulo "Playfair" eh carregado com
-                        # sucesso.
-#}
-
-#BEGIN {
-  # Se fosse classe:
-  #my $obj = Playfair->novo();  # Instancia um novo objeto
-  #isa_ok $obj, "Playfair";     # Testa se instanciou corretamente
-
-  #is $obj->decrypt, "Dae\n", "Decrypt funciona";  # Testa retorno da funcao
-  is Playfair::decrypt(), "Dae\n", "Decrypt funciona";  # Testa retorno da funcao
-
-  # Testando a geracao das chaves, passando um tamanho maximo de chaves:
-  my $MAXLEN  = 6;
-  #my @meta_chaves = $obj->gera_meta_chaves($MAXLEN);
-  my @meta_chaves = Playfair::gera_meta_chaves($MAXLEN);
-  is @meta_chaves, $MAXLEN, "Meta-chave com tamanho correto";
-
-  # Testando a separacao das strings.
-  #is $obj->split("asdf"), "as df",  "asdf separado corretamente";
-  #is $obj->split("aasd"), "ax as dx", "'aasd' separado corretamente";
-
-#}
+# Importacao do modulo:
+require_ok("Playfair");
 
 
+# Testando a geracao das chaves, passando um tamanho maximo de chaves:
+my $MAXLEN  = 6;
+my $meta_chaves_ref = Playfair::gera_meta_chaves($MAXLEN);
+is @$meta_chaves_ref, $MAXLEN, "Meta-chave com tamanho correto";
+
+
+# Testando a geracao da matriz de decriptografia, dada uma chave qualquer.
+# Note que escolhi representar uma matriz por uma string, pq preferi trabalhar
+# direto com os indices.
+is  Playfair::gera_matriz("ZAraFa"),
+    "zarfbcdeghiklmnopqstuvwxy",
+    "Teste 1 de geracao de matriz";
+
+is  Playfair::gera_matriz("zaRafA"),
+    "zarfbcdeghiklmnopqstuvwxy",
+    "Teste 2 de geracao de matriz";
+
+is  Playfair::gera_matriz("XerxEs"),
+    "xersabcdfghiklmnopqtuvwyz",
+    "Teste 3 de geracao de matriz";
+
+is  Playfair::gera_matriz("xerxes"),
+    "xersabcdfghiklmnopqtuvwyz",
+    "Teste 4 de geracao de matriz";
+
+# Level carioca
+is  Playfair::gera_matriz("XehRrxish"),
+    "xehrisabcdfgklmnopqtuvwyz",
+    "Teste 5 de geracao de matriz";
+
+
+# A funcao decrypt deve criar um arquivo com o conteudo decifrado. Como isso
+# nao eh mto simples de ser testado, fazemos o teste pela existencia do
+# arquivo, verificando se ele possui o nome correto e o conteudo tambem, para
+# tanto, pegamos uma palavra cifrada e a deciframos, e entao lemos do arquivo
+# de saida e comparamos com o resultado esperado.
+
+my $chave = 'camisa';
+my $arq   = 'teste_camisa';
+my $crypt = "rhiwdimt";     # Note q o texto passado para a funcao jah vem sem
+                            # os espacos entre os caracteres.
+
+Playfair::decrypt($chave,$crypt,$arq);
+
+ok( -f $arq, "Decrypt cria o arquivo de saida");
+open(my $arq_read, "<", $arq);
+
+my $result = '';
+while(<$arq_read>) {
+
+  $result .= $_;
+}
+
+is $result, "pl ay fa ir \n", "Decifrado com sucesso";
+is `rm -f $arq`, '', "Arquivo teste removido com sucesso.";
 
 done_testing();
