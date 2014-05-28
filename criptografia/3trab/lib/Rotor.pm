5e17e955cbb2d3758d9347b4c7d2ecd7d0a9695b
aramisf <ashf03@c3sl.ufpr.br>
2014-05-28 16:57:37 -0300

    [cripto] Refatoracao.

--- criptografia/3trab/lib/Rotor.pm
+++ criptografia/3trab/lib/Rotor.pm
@@ -14,6 +14,42 @@ use warnings;
 
 package Rotor;
 
+
+  # Por convencao, a galera coloca _ antes dos nomes, mas o metodo aqui se
+  # torna privado porque esta sendo referenciado na variavel $desloca, que eh
+  # visivel apenas dentro do escopo local, ou seja, apenas pelos metodos
+  # internos da Classe.
+  # TODO: 'our'?
+  my $desloca = sub {
+
+    my ($eu,$deslocamento,$txt)  = @_;
+    my $deslocado;
+
+    if ($deslocamento eq '+') {
+      for (split //, $txt) {
+
+        $deslocado  .= chr ((ord) + $eu->{DESLOCAMENTO});
+      }
+
+      $eu->{CIFRADO}  = $deslocado;
+    }
+
+    # XXX: Quando isso aqui for ativado, tem que inverter a ordem dos rotores.
+    elsif ($deslocamento eq '-') {
+
+      for (split //, $txt) {
+
+        $deslocado  .= chr ((ord) - $eu->{DESLOCAMENTO});
+      }
+
+      $eu->{DECIFRADO}  = $deslocado;
+    }
+
+  };  #/my $desloca
+
+
+  # Ae, metodos publicos;
+
   sub sumona {
 
     my ($classe,$deslocamento) = @_;
@@ -29,15 +65,13 @@ package Rotor;
 
   sub cifra {
 
-    my ($eu,$txt)  = @_;
-    my $cifrado;
-
-    for (split //, $txt) {
-
-      $cifrado  .= chr ((ord) + $eu->{DESLOCAMENTO});
-    }
-
-    $eu->{CIFRADO}  = $cifrado;
+    my ($eu, $txt)  = @_;
+    $eu->$desloca('+', $txt);
   }
 
+  sub decifra {
+
+    my ($eu, $txt)  = @_;
+    $eu->$desloca('-', $txt);
+  }
 1;
--- criptografia/3trab/maq.pl
+++ criptografia/3trab/maq.pl
@@ -5,7 +5,7 @@ use warnings;
 
 ################################################################################
 # Os parametros tem o seguinte formato:                                        #
-# ./$0 z1 z2 z3 z4 z5 arq.txt                                                  #
+# ./$0 z1..z5 arq.txt                                                          #
 #                                                                              #
 # Onde:                                                                        #
 # - z[1-5] sao numeros inteiros;                                               #
@@ -15,14 +15,14 @@ use warnings;
 
 use Rotor;
 
-sub uso {
-
-  print "Uso:\n\t$0 z1..zn <arquivo texto>\n";
-  exit 1;
-}
-
-uso() if not @ARGV;
-uso() if $ARGV[-1] and not -f $ARGV[-1];
+#sub uso {
+#
+#  print "Uso:\n\t$0 z1..zn <arquivo texto>\n";
+#  exit 1;
+#}
+#
+#uso() if not @ARGV;
+#uso() if $ARGV[-1] and not -f $ARGV[-1];
 
 
 my $rotor = Rotor->sumona(2);
